28b99cb69fe782498a2cba91d1ecf14126f6df5b
srbshameer098 <srbshameer098@gmail.com>
2023-11-22 12:27:45 +0530

    home2

--- lib/UI/Home.dart
+++ lib/UI/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -9,24 +10,103 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-appBar: AppBar(
-  elevation: 8,
-  leading: Icon(Icons.menu),
-  title: Container(
-    width: 150,height: 30,
-      child: Row(
-    children: [
-      SizedBox(width: 30,height: 20,
-        child: Image.asset("assets/Brand.png",
-          width: 5,
-          height: 5,),
+
+    return  DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        backgroundColor: Color(0xffc3dcf1),
+      appBar: AppBar(
+        backgroundColor: Color(0xff13161c),
+        elevation: 1,
+        leading: Icon(Icons.menu,color: Colors.white,),
+        title: Container(
+      width: 150.w,height: 30.h,
+        child: Row(
+      children: [
+        SizedBox(width: 40.w,height: 30.h,
+          child: Image.asset("assets/Brand.png",
+            width: 5.w,
+            height: 5.h,),
+        ),
+        SizedBox(width: 10.w,),
+        Text("Brand",style: TextStyle(
+          color: Colors.white
+        ),),
+      ],
+        )),
       ),
-      SizedBox(width: 10,),
-      Text("Brand"),
-    ],
-  )),
-),
+        body:  SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                Row(
+                  children: [
+                    Container(
+                      height: 40.h,
+               decoration: BoxDecoration(color: Color(0xffded9f1), borderRadius: BorderRadius.circular(12.r),),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text('All Category',style: TextStyle(
+                            color: Color(0xff926aed),
+                          ),),
+                        ),
+                      ),
+                    ),
+SizedBox(width: 10.w,),
+                    Container(
+                      height: 40.h,
+                      decoration: BoxDecoration(color: Color(0xffded9f1), borderRadius: BorderRadius.circular(12.r),),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text('Gadgets',style: TextStyle(
+                            color: Color(0xff926aed),
+                          ),),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10.w,),
+                    Container(
+                      height: 40.h,
+                      decoration: BoxDecoration(color: Color(0xffded9f1), borderRadius: BorderRadius.circular(12.r),),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text('Cloths',style: TextStyle(
+                            color: Color(0xff926aed),
+                          ),),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10.w,),
+                    Container(
+                      height: 40.h,
+                      decoration: BoxDecoration(color: Color(0xffded9f1), borderRadius: BorderRadius.circular(12.r),),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text('Accessory',style: TextStyle(
+                            color: Color(0xff926aed),
+                          ),),
+                        ),
+                      ),
+                    ),
+
+                  ],
+                )
+
+
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+
   }
 }
