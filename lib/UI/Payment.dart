b8bbc2ba458fb2d8f3e543d6bd1a7e3192c03442
srbshameer098 <srbshameer098@gmail.com>
2024-01-23 18:42:23 +0530

    add_payment

---
+++ lib/UI/Payment.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class Payment extends StatefulWidget {
+  const Payment({Key? key}) : super(key: key);
+
+  @override
+  State<Payment> createState() => _PaymentState();
+}
+
+class _PaymentState extends State<Payment> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+    );
+  }
+}
